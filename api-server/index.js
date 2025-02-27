4e1c07e7f69cbad6b0787129c78cc220ea8f2007
amasakunika <saku.amaa93@gmail.com>
2025-02-27 23:39:55 +0530

    bootcamp day one assignment

--- api-server/index.js
+++ api-server/index.js
@@ -2,7 +2,8 @@ const express=require("express");
 
 const {getMinNumber} = require('./util.js');
 const {getMaxNumber} = require('./util.js');
-const {averageNumbers} = require('./util.js');
+const {getMaxFromNumbers} = require('./util.js');
+const {getAverageValue} = require('./util.js');
 const {sortNumbers} = require('./util.js');
 const {countOccurrance} = require('./util.js');
 
@@ -21,13 +22,13 @@ app.get('/number/min', (req,res) => {
 
    const result = getMinNumber(num1,num2);
    
-   res.status(result.status).json(result.data); 
+   res.status(result.status).json(result.data);
 
    //if(isNaN(num1) || isNaN(num2)){
       //res.json({message: "error"});
    //}
    //return null instead of NaN as json conver NaN to null as NaN is not a value in json
-   //res.json({min: num1 > num2 ? num2 : num1}); 
+   //res.json({min: num1 > num2 ? num2 : num1});
    //OR
    //res.json({min: Math.min(num1, num2)});
 });
@@ -37,23 +38,47 @@ app.get('/number/max', (req,res) => {
     const num2 = parseFloat(req.query.num2);
  
     const result = getMaxNumber(num1,num2);
+   
+    res.status(result.status).json(result.data);
+});
+
+app.get('/number/maximum', (req,res) => {
+    const numbers = req.query.numbers.split(',').map(num => parseFloat(num));
     
-    res.status(result.status).json(result.data); 
+    const result = getMaxFromNumbers(numbers);
+   
+    res.status(result.status).json(result.data);
 });
 
 app.get('/number/avg', (req,res) => {
-    const arr1 = parseFloat(req.query.num1);
-    const num2 = parseFloat(req.query.num2);
+    const numbers = req.query.numbers.split(',').map(num => parseFloat(num));
  
-    const result = getMaxNumber(num1,num2);
-    
-    res.status(result.status).json(result.data); 
+    const result = getAverageValue(numbers);
+   
+    res.status(result.status).json(result.data);
+});
+
+app.get('/number/sort', (req,res) => {
+    const numbers = req.query.numbers.split(',').map(num => parseFloat(num));
+ 
+    const result = sortNumbers(numbers);
+   
+    res.status(result.status).json(result.data);
+});
+
+app.get('/number/count', (req,res) => {
+    const values = req.query.values.split(',').map(value => parseFloat(value));
+    const search = parseFloat(req.query.search);
+ 
+    const result = countOccurrance(values,search);
+   
+    res.status(result.status).json(result.data);
 });
 
 //app.get('/number/max', (req,res) => {});
 //app.get('/number/avg', (req,res) => {}); //number/avg?numbers=1,4,7,44,676,....
-app.get('/number/sort', (req,res) => {}); //number/sort?numbers=1,4,7,44,676,....n&type(asc | dec)
-app.get('/number/count', (req,res) => {}); //number/count?numbers=1,4,G,Kamal,G,....n&search=G //need to search how many occurences
+//app.get('/number/sort', (req,res) => {}); //number/sort?numbers=1,4,7,44,676,....n&type(asc | dec)
+//app.get('/number/count', (req,res) => {}); //number/count?numbers=1,4,G,Kamal,G,....n&search=G //need to search how many occurences
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
--- api-server/util.js
+++ api-server/util.js
@@ -33,19 +33,87 @@ function getMaxNumber(num1,num2){
     };
 }
 
-function getAverageNumber(numbers){
-    
+function getMaxFromNumbers(numbers){
+
+
+    if(numbers.some(isNaN)){
+        return {
+            status: 400,
+            data: {
+                error: "Please provide all numbers in valid type"
+            },
+        };
+    };
+
+    return{
+        status: 400,
+        data: {
+            max: Math.max(...numbers)
+        },
+    };
+}
+
+function getAverageValue(numbers){
+    if(numbers.some(isNaN)){
+        return {
+            status: 400,
+            data: {
+                error: "Please provide all numbers in valid type"
+            },
+        };
+    };
+
+    let sum = numbers.map(number => sum += number);
+
+    return{
+        status: 400,
+        data: {
+            avg: sum / numbers.length
+        },
+    };
 }
 
 function sortNumbers(numbers){
-    
+    if(numbers.some(isNaN)){
+        return {
+            status: 400,
+            data: {
+                error: "Please provide all numbers in valid type"
+            },
+        };
+    };
+
+    return{
+        status: 400,
+        data: {
+            type: "Ascending Order",
+            sort: numbers.sorted()
+        },
+    };
 }
 
-function countOccurrance(numbers){
-    
+function countOccurrance(values,search){
+    if(!values || !search){
+        return {
+            status: 400,
+            data: {
+                error: "Please provide both values and search occurrance"
+            },
+        };
+    };
+    return{
+        status: 400,
+        data: {
+            count: values.count(search)
+        },
+    };
 }
 
 module.exports = {
     getMinNumber,
-    getMaxNumber
+    getMaxNumber,
+    getMaxFromNumbers,
+    getAverageValue,
+    sortNumbers,
+    countOccurrance
 };
